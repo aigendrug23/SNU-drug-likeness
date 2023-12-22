8268f974083be715955d0e38bdeac74997172d41
kimkun07 <kimkun07@naver.com>
2023-12-22 10:58:01 +0900

    aigendrug1.2

--- demo/test.py
+++ demo/test.py
@@ -1,7 +1,7 @@
 from src.inference import get_prediction
 from src.model_config import get_model_admet, get_model_dcc, get_chem_space
 from src.prediction_xgboost import predict_single_vector
-from src.prediction_admet import get_embedding_vector
+from src.prediction_admet import get_embedding_vector, get_adme_from_vector
 from src.dataset_mtl import smilesToGeometric
 import torch
 import pandas as pd
@@ -24,31 +24,29 @@ for i in df.index:
     smiles = df.loc[i, "SMILES"]
     embedding_vector = df.loc[i, "0":"511"]
 
-    # embedding_vector = embedding_vectors.iloc[i]
-    embedding_vector_chem = embedding_vector.values.reshape(1, -1)
-    drugLikeness_chem = predict_single_vector(dccModel, embedding_vector_chem)
-
     data = smilesToGeometric(smiles)
     data.batch = torch.tensor([0] * len(data.x))
     # Embedding Vector: torch.Size([1, 512])
-    embedding_vector_model = get_embedding_vector(encoder, data)
+    embedding_vector = get_embedding_vector(encoder, data)
 
-    print(smiles)
-    print(embedding_vector_chem[0][:5])
-    print(embedding_vector_model.detach().numpy()[0][:5])
+    # ADME Prediction
+    result_bbb, result_cyp, result_sol, result_clr = get_adme_from_vector(
+        predictionHead, embedding_vector
+    )
 
-    break
     drugLikeness = predict_single_vector(dccModel, embedding_vector.detach().numpy())
 
-    if drugLikeness < 0.4:
-        ls.append((smiles, drugLikeness))
-    if abs(drugLikeness_chem - drugLikeness) > 0.3:
-        ls2.append((smiles, drugLikeness_chem, drugLikeness))
+    if 0.3 < drugLikeness < 0.4 and 0.2 < result_bbb < 0.5:
+        ls.append(
+            (smiles, drugLikeness, result_bbb, result_cyp, result_sol, result_clr)
+        )
+    if 0.6 < drugLikeness < 0.8 and 0.2 < result_bbb < 0.5:
+        ls2.append(
+            (smiles, drugLikeness, result_bbb, result_cyp, result_sol, result_clr)
+        )
 
 
-for smi, drug in ls:
-    if drug < 0.4:
-        print(smi, drug)
-
-print(len(ls))
-print(len(ls2))
+for smi, drug, b, c, s, clr in ls:
+    print(f"{smi}: {drug} for BBB: {b}, CYP: {c}, SOL: {s}, CLR: {clr}")
+for smi, drug, b, c, s, clr in ls2:
+    print(f"{smi}: {drug} for BBB: {b}, CYP: {c}, SOL: {s}, CLR: {clr}")
